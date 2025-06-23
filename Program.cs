7e704d1217654b1fd72a40d6f1c2ca2947baba0d
Trần Phương Huyền <tranphuonghuyen2005@gmail.com>
2025-06-23 14:23:17 +0700

    add test

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Greeter
+{
+    public string GetGreeting() => "Hello, World!";
+}
+
+class Program
+{
+    static void Main()
+    {
+        var greeter = new Greeter();
+        for (int i = 0; i < 4; i++)
+            Console.WriteLine(greeter.GetGreeting());
+    }
+}
